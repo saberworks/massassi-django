4cc6181f9c7bb959d9201b342a1b7a5b0cc70a29
Brian E. Lozier <brian@massassi.com>
2021-03-22 21:38:01 -0700

    fix level importer
    massassi db had some filenames with htmlentity for apostrophe/single quote
    but on the filesystem it was an actual single quote; fix that during import
    
    report and skip importing of levels with missing files; those should be fixed
    manually before the real/production import

--- levels/management/commands/import_levels.py
+++ levels/management/commands/import_levels.py
@@ -1,4 +1,6 @@
 import os
+import re
+
 import pytz
 
 from datetime import datetime
@@ -16,6 +18,8 @@ class Command(OurMySqlImportBaseCommand):
 
         parser.add_argument('--ss_path', nargs='?', type=str, required=True, help='Full path to screenshot directory')
         parser.add_argument('--files_path', nargs='?', type=str, required=True, help='Path to level files')
+        parser.add_argument('--level_id', nargs='?', type=int, required=False, help='Optional level id')
+        parser.add_argument('--start_level_id', nargs='?', type=int, required=False, help='Start at this level id')
 
     def handle(self, *args, **options):
         cnx = self.get_connection(options)
@@ -24,18 +28,37 @@ class Command(OurMySqlImportBaseCommand):
 
         cursor = cnx.cursor(dictionary=True)
 
-        query = "SELECT * FROM levels ORDER BY level_id"
+        query = 'SELECT * FROM levels ORDER BY level_id'
+        query_params = ()
 
-        cursor.execute(query)
+        if options['level_id']:
+            query = 'SELECT * FROM levels WHERE level_id = %s'
+            query_params = (options['level_id'],)
+
+        if options['start_level_id']:
+            query = 'SELECT * FROM levels WHERE level_id >= %s'
+            query_params = (options['start_level_id'],)
+
+        cursor.execute(query, query_params)
 
         for row in cursor.fetchall():
             level_id = row['level_id']
             category = categories[row['category_id']]
 
+            self.stdout.write("Processing {} ({})".format(level_id, category))
+
             screenshot_1 = self.get_screenshot(options['ss_path'], level_id, 1)
             screenshot_2 = self.get_screenshot(options['ss_path'], level_id, 2)
 
-            file = self.get_file(options['files_path'], category.path, row['file_name'])
+            # filenames appear to be messed up with html entities, try to
+            # replace them with the actual characters that are on the
+            # filesystem
+            file_name = re.sub(r'&#39;', "'", row['file_name'])
+
+            file = self.get_file(options['files_path'], category.path, file_name, level_id)
+
+            if not file:
+                continue
 
             date_created = datetime.fromtimestamp(
                 row['timestamp'], tz=pytz.timezone('America/Los_Angeles')
@@ -57,17 +80,15 @@ class Command(OurMySqlImportBaseCommand):
                 rating=rating,
             )
 
-            level.file.save(row['file_name'], file, save=True)
+            level.file.save(file_name, file, save=False)
 
             if screenshot_1:
-                level.screenshot_1.save("{}_{}.jpg".format(level_id, 1), screenshot_1, save=True)
+                level.screenshot_1.save("{}_{}.jpg".format(level_id, 1), screenshot_1, save=False)
 
             if screenshot_2:
-                level.screenshot_2.save("{}_{}.jpg".format(level_id, 2), screenshot_2, save=True)
+                level.screenshot_2.save("{}_{}.jpg".format(level_id, 2), screenshot_2, save=False)
 
             level.save()
-
-            self.stdout.write("Processed {}".format(level_id))
 
         cursor.close()
         cnx.close()
@@ -76,17 +97,17 @@ class Command(OurMySqlImportBaseCommand):
         ss_file = "{}/{}_{}.jpg".format(ss_path, level_id, ss_number)
 
         if not os.path.isfile(ss_file):
-            self.stderr.write("Screenshot for {} missing from filesystem; skipping".format(ss_file))
+            self.stdout.write("Screenshot for {} missing from filesystem; skipping".format(ss_file))
             return None
 
         fh = open(ss_file, 'rb')
         return File(fh)
 
-    def get_file(self, files_path, category_path, file_name):
+    def get_file(self, files_path, category_path, file_name, level_id):
         file = "{}/{}/{}".format(files_path, category_path, file_name)
 
         if not os.path.isfile(file):
-            self.stderr.write("File for {} missing from filesystem; skipping".format(file))
+            self.stderr.write("File for level id {} missing from filesystem ({}); skipping".format(level_id, file))
             return None
 
         fh = open(file, 'rb')
