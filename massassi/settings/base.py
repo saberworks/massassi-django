22e1823e2ee0eda28fab9d2c08f68e1e79e67847
Brian E. Lozier <brian@massassi.com>
2021-12-06 19:10:33 -0800

    fix from email

--- massassi/settings/base.py
+++ massassi/settings/base.py
@@ -136,6 +136,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 SENDGRID_API_KEY = get_env_variable('SENDGRID_API_KEY')
 
+DEFAULT_FROM_EMAIL = 'The Massassi Temple <massassi.temple@gmail.com>'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
