6d3568154b94068347b8141ad985e255e519f0e2
Brian E. Lozier <brian@massassi.com>
2021-12-12 16:13:04 -0800

    forms change to fix registration
    database_info.sh used for data import (the various management import scripts)

---
+++ bin/database_info.sh
@@ -0,0 +1,6 @@
+# These are used for importing the old database.  Once that is finished
+# in production, this file can be removed.
+export MYSQL_HOST=$MASSASSI_DATABASE_HOST
+export MYSQL_USER=massassi
+export MYSQL_PASSWORD=$MASSASSI_DATABASE_PASSWORD
+export MYSQL_DATABASE=massassi
