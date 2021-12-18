471bb07a275a4d9f76be3b1cfe64ed3fa46898e7
Brian E. Lozier <brian@massassi.com>
2021-12-18 13:34:25 -0800

    fix import db host (should point to the mysql one)

--- bin/database_info.sh
+++ bin/database_info.sh
@@ -1,6 +1,6 @@
 # These are used for importing the old database.  Once that is finished
 # in production, this file can be removed.
-export MYSQL_HOST=$MASSASSI_DATABASE_HOST
+export MYSQL_HOST=db-import
 export MYSQL_USER=massassi
 export MYSQL_PASSWORD=$MASSASSI_DATABASE_PASSWORD
 export MYSQL_DATABASE=massassi
