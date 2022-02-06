6773d1b962faaa76530f154c376d824407c6d217
Brian E. Lozier <brian@massassi.com>
2022-02-05 18:44:12 -0800

    stop deleting migration files
    now that we're live, no resetting the migration files from scratch anymore

--- bin/wipedb.sh
+++ bin/wipedb.sh
@@ -2,8 +2,5 @@
 set -euxo pipefail
 
 # WIPE DATABASE
-find ../ -path '*/migrations/*' -regex ".*/[0-9]+.*" -delete
-find ../ -name '*.pyc' -delete
-
 dropdb -h db -p 5432 -U massassi massassi
 createdb -h db -p 5432 -U massassi --owner=massassi massassi
