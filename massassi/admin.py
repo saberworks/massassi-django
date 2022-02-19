ac5217fe46b76ec3d28b0c30b7858b337b702897
Brian E. Lozier <brian@massassi.com>
2022-02-19 00:34:45 -0800

    auto-populate `user`
    also more robust handling when inheriting from the base model

--- massassi/admin.py
+++ massassi/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 
 class MassassiModelAdmin(admin.ModelAdmin):
     def get_fields(self, request, obj=None):
-        return self.fields + ('created_at', 'created_by', 'last_modified_at', 'last_modified_by')
+        add_fields = ('created_at', 'created_by', 'last_modified_at', 'last_modified_by')
+        if(self.fields):
+            return self.fields + add_fields
+        else:
+            return add_fields
 
     # These fields should be shown on the form, but should be readonly
     def get_readonly_fields(self, request, obj=None):
@@ -11,12 +15,17 @@ class MassassiModelAdmin(admin.ModelAdmin):
 
     # On save, set the created_by/last_modified_by as appropriate
     def save_model(self, request, obj, form, change):
+        # some user-editable tables also have a `user` field; if it's present
+        # populate from the request
+        for field in obj._meta.get_fields():
+            if field.name == 'user':
+                obj.user = request.user
+
         if change:
             obj.last_modified_by = request.user
         else:
             obj.created_by = request.user
             obj.last_modified_by = request.user
-
 
         obj.save()
 
