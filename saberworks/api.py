2d8f787a3c433e557d5563a3d054aabfcd689e19
Brian E. Lozier <brian@massassi.com>
2022-02-21 00:49:18 -0800

    add ability to create new projects via api
    image field is currently allowing any file type to be uploaded, must fix

--- saberworks/api.py
+++ saberworks/api.py
@@ -1,12 +1,16 @@
 import logging
+import re
 
+from django.db import Error
 from django.shortcuts import get_list_or_404, get_object_or_404
-from ninja import Router
+from ninja import Router, File as NinjaFile
+from ninja.files import UploadedFile
 from ninja.security import django_auth
-from pydantic.typing import List
+from pydantic.typing import List, Optional
 
 from .models import File, Game, Post, Project, Tag, TagType
 from .schemas import FileOut, GameOut, PostOut, ProjectListOut, ProjectOut, TagOut, TagTypeOut
+from .schemas import ProjectIn, NewProjectOut
 
 logger = logging.getLogger(__name__)
 
@@ -29,10 +33,16 @@ def get_tags(request):
 # logged in user
 #
 
+###
+## Get a list of projects
+#
 @router.get("/projects", response=List[ProjectListOut], auth=django_auth)
 def get_projects(request):
     return get_list_or_404(Project, user=request.user)
 
+###
+## Get a single project
+#
 @router.get("/project/{project_id}", response=ProjectOut, auth=django_auth)
 def get_project(request, project_id: int):
     return get_object_or_404(
@@ -42,8 +52,51 @@ def get_project(request, project_id: int):
                .prefetch_related("games"),
         id=project_id, user=request.user)
 
+###
+## Create a new project
+#
+@router.post("/projects", response=NewProjectOut, auth=django_auth)
+def add_project(request, payload: ProjectIn, image: Optional[UploadedFile] = NinjaFile(None)):
+    project = payload.dict()
+
+    games = project.pop('games', None)
+    tags = project.pop('tags', None)
+
+    project['user'] = request.user
+
+    new_project = Project.objects.create(**project)
+
+    messages = []
+
+    # TODO this image handling allows putting any file as the image and django
+    # model ImageField doesn't complain.  Need to do validation that
+    # the thing uploaded is actually a valid image
+    if image is not None:
+        new_project.image = image
+        new_project.save()
+
+    if isinstance(games, list):
+        try:
+            [new_project.games.add(game) for game in games]
+        except Error as error:
+            if not re.search("foreign key constraint", str(error)):
+                raise
+            else:
+                messages.append(get_friendly_invalid_fk_error_message(str(error)))
+
+    if isinstance(tags, list):
+        try:
+            [new_project.tags.add(tag) for tag in tags]
+        except Error as error:
+            if not re.search("foreign key constraint", str(error)):
+                raise
+            else:
+                messages.append(get_friendly_invalid_fk_error_message(str(error)))
+
+    return { "project": new_project, "messages": messages }
+
 # TODO:
-# POST add project (allow specifying of tags, games)
+# validate image uploads are actually images
 # PUT edit project
 # DELETE delete project
 
@@ -72,3 +125,15 @@ def get_file_for_project(request, project_id, file_id):
 # POST add file (allow image & file upload somehow)
 # PUT edit file (allow replacement image? or delete image? or replace file? gah)
 # DELETE delete file
+
+# Given a databse error message from a foreign key constraint, extract the
+# offending id and return it
+def get_friendly_invalid_fk_error_message(error):
+    result = re.search("\((\w+)\)=\((\d+)\)", error)
+
+    if result is not None:
+        [field, id] = result.groups()
+
+        return "Error: {} is an invalid {}.".format(id, field)
+
+    return "Some database error happened but I don't know what, sorry!"
--- saberworks/schemas.py
+++ saberworks/schemas.py
@@ -1,18 +1,23 @@
 import logging
 
-from ninja import ModelSchema
+from ninja import Schema, ModelSchema
+from ninja.files import UploadedFile
 from pydantic.typing import List, Optional
 from pydantic import fields
 
 from users.models import User
 from .models import File, Game, Post, Project, Tag, TagType
 
-# very restricted, just send back id and username
+# UserOut schema is very restricted, just send back id and username
 class UserOut(ModelSchema):
     class Config:
         model = User
         model_fields = ['id', 'username']
 
+
+###
+## Game Schemas
+#
 
 class GameOut(ModelSchema):
     image: Optional[str]
@@ -22,6 +27,10 @@ class GameOut(ModelSchema):
         model_fields = ['id', 'name', 'slug', 'description', 'image']
 
 
+###
+## Tag Type Schemas
+#
+
 class TagTypeOut(ModelSchema):
     tag_type: str = fields.Field(None, alias='slug')
 
@@ -29,6 +38,9 @@ class TagTypeOut(ModelSchema):
         model = TagType
         model_fields = ['id',]
 
+###
+## Tag Schemas
+#
 
 class TagOut(ModelSchema):
     type: str = fields.Field(None, alias="type.slug")
@@ -37,6 +49,11 @@ class TagOut(ModelSchema):
     class Config:
         model = Tag
         model_fields = ['id',]
+
+
+###
+## Project Schemas
+#
 
 # This is here so one can request a list of projects and not cause a bunch of
 # cascading queries.  It's similar to ProjectOut below except that all the
@@ -50,6 +67,15 @@ class ProjectListOut(ModelSchema):
         model = Project
         model_fields = ['id', 'user', 'name', 'slug', 'description', 'accent_color', 'image']
 
+class ProjectIn(ModelSchema):
+    games: List[int] = [] # list of game IDs
+    tags: List[int] = []  # list of tag IDs
+    image: Optional[UploadedFile] = None
+
+    class Config:
+        model = Project
+        model_fields = ['games', 'tags', 'name', 'description', 'accent_color', 'image']
+
 class ProjectOut(ModelSchema):
     games: List[GameOut] = []
     tags: List[TagOut] = []
@@ -59,6 +85,14 @@ class ProjectOut(ModelSchema):
         model = Project
         model_fields = ['id', 'user', 'games', 'tags', 'name', 'slug', 'description', 'accent_color', 'image']
 
+class NewProjectOut(Schema):
+    project: ProjectOut
+    messages: Optional[List[str]] = []
+
+
+###
+## Post Schemas
+#
 
 class PostOut(ModelSchema):
     image: Optional[str]
@@ -66,6 +100,11 @@ class PostOut(ModelSchema):
     class Config:
         model = Post
         model_fields = ['id', 'user', 'project', 'title', 'slug', 'text', 'image']
+
+
+###
+## File Schemas
+#
 
 class FileOut(ModelSchema):
     image: Optional[str]
