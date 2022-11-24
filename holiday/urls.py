44c48b729b78c8bf40adda51b4eddc21d29ab68e
Brian E. Lozier <brian@massassi.com>
2022-11-23 18:27:53 -0800

    endpoint for random holiday url

--- holiday/urls.py
+++ holiday/urls.py
@@ -6,5 +6,6 @@ app_name = 'holiday'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('random/', views.RandomUrlView.as_view(), name='random'),
     path('<int:year>/', views.YearView.as_view(), name='year'),
 ]
